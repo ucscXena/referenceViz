0a8aace2e03eb06b7ec861d64607ee8c8a836661
Brian Craft <bcboy@thecraftstudio.com>
2025-10-17 14:45:05 -0700

    Add theme and css baseline

--- js/demo.js
+++ js/demo.js
@@ -3,8 +3,13 @@ import parseURL from './parseURL';
 import ReactDOM from 'react-dom';
 import singlecellView from './singlecellView';
 import singlecellLegend from './singlecellLegend';
-import {div} from './react-hyper';
+import {div, el} from './react-hyper';
 import styles from './demo.module.css';
+import {MuiThemeProvider, CssBaseline} from '@material-ui/core';
+import {xenaTheme as theme} from './theme';
+
+var muiThemeProvider = el(MuiThemeProvider);
+var cssBaseline = el(CssBaseline);
 
 var {path, params} = parseURL(window.location.href);
 var segments = path.slice(1).replace(/\/$/, '').split(/\//);
@@ -12,16 +17,17 @@ var segments = path.slice(1).replace(/\/$/, '').split(/\//);
 if (segments[0] === 'pyramid') {
 	var main = document.getElementById('main');
 	main.style.position = 'relative';
-	document.body.style.margin = '0';
 	var state = {/*showColorPicker: true,*/ layer: 0}, onState;
 	var render = () => {
 		ReactDOM.render(
-			div({className: styles.singlecell},
-				singlecellView({
-					image: params.image,
-					state,
-					onState}),
-				singlecellLegend(state, onState)),
+			muiThemeProvider({theme},
+				cssBaseline(),
+				div({className: styles.singlecell},
+					singlecellView({
+						image: params.image,
+						state,
+						onState}),
+					singlecellLegend(state, onState))),
 			main);
 	};
 	onState = fn => {
