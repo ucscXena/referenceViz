93c21af3b533536de7b8d0ace4ff6de276092326
Brian Craft <bcboy@thecraftstudio.com>
2025-10-17 13:52:31 -0700

    Fix flex resizing

--- js/demo.js
+++ js/demo.js
@@ -11,9 +11,8 @@ var segments = path.slice(1).replace(/\/$/, '').split(/\//);
 
 if (segments[0] === 'pyramid') {
 	var main = document.getElementById('main');
-	main.style.width = '100%';
-	main.style.height = '1000px';
 	main.style.position = 'relative';
+	document.body.style.margin = '0';
 	var state = {/*showColorPicker: true,*/ layer: 0}, onState;
 	var render = () => {
 		ReactDOM.render(
