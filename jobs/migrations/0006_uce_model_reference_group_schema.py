5d91df980e7e19c28556185ac15e9ae3769f0d45
Brian Craft <bcboy@thecraftstudio.com>
2026-04-08 10:10:38 -0700

    update migrations

--- jobs/migrations/0006_uce_model_reference_group_schema.py
+++ jobs/migrations/0006_uce_model_reference_group_schema.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='UCEModel',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False)),
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(max_length=100, unique=True)),
                 ('model_url', models.CharField(max_length=500)),
                 ('is_default', models.BooleanField(default=False)),
